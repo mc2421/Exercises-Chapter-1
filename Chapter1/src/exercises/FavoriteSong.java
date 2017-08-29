e651977425e3842f73ddae052ad5b29a8e45dcb7
mc307009 <michaelcurtis2421@gmail.com>
2017-08-29 09:27:04 -0500

    Chapter-1 Excerises

---
+++ Chapter1/src/exercises/FavoriteSong.java
@@ -0,0 +1,13 @@
+package exercises;
+
+import javax.swing.JOptionPane;
+
+public class FavoriteSong {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null,"");
+
+	}
+
+}
